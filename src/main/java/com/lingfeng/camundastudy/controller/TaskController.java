ba2424852f6ea2428f1d530c1a25a1006fce6ed9
zb <jackzb.zeng@relxintl.com>
2025-12-07 16:43:56 +0800

    feat(camunda): 配置MySQL数据源并新增流程定义及启动控制器
    - 配置application.yml使用MySQL数据库连接池
    - 添加spring-boot-starter-jdbc和MySQL驱动依赖
    - 新增两个BPMN流程文件simple-process和diagram_1
    - 创建TaskController启动名为diagram_1的流程实例
    - 设置日志级别，开启自动部署功能及资源匹配模式

---
+++ src/main/java/com/lingfeng/camundastudy/controller/TaskController.java
@@ -0,0 +1,22 @@
+package com.lingfeng.camundastudy.controller;
+
+import jakarta.annotation.Resource;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    @Resource
+    private RuntimeService runtimeService;
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("diagram_1");
+    }
+}
